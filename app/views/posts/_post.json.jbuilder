c02514799edf5eecfbcb2dae3ac8686cd23ab347
Josh Marchello <josh@jmarchello.com>
2022-01-15 17:15:54 +0000

    basic posts CRUD

--- app/views/posts/_post.json.jbuilder
+++
@@ -1,3 +0,0 @@
-json.extract! post, :id, :content, :title, :published_at, :created_at, :updated_at
-json.url post_url(post, format: :json)
-json.content post.content.to_s
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
+  namespace :admin do
+    resources :posts, only: %i[new edit update create destroy]
+  end
+
+  resources :posts, only: %i[show index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
