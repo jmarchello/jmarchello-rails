45e248fc57b1c58374128aefd1eb9e2f249bdda6
Josh Marchello <josh@jmarchello.com>
2022-01-15 18:50:01 +0000

    fixed auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :lockable, :rememberable, :trackable
+  devise :database_authenticatable, :lockable, :rememberable
 end
